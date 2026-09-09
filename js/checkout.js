785d89c0e4c11a5f3cba83396944b856bde1396f
abell777 <178426413+abell777@users.noreply.github.com>
2026-09-09 12:04:50 +0200

    PK

--- js/checkout.js
+++ js/checkout.js
@@ -29,7 +29,7 @@
     if (!cardElementContainer) return;
 
     // Sustituye con tu Clave Publicable de Stripe (pk_live_... o pk_test_...)
-    stripe = Stripe('pk_test_51UADO2L8aFj5g4tzYon8DA5gIizvUtSPZq5y6NNiU7EIfccqmxEPET6KDxVTwfZ2dxTzmwmAJDGZ2G2Vzy8jSgAi002G7ZSP9f');
+    stripe = Stripe('pk_live_51UADO2L8aFj5g4tzzrr2Hu6XSBy9z9rRY6ooWgOgXxDz7DxqnD0geQba6HnKVJfIQ0g1jyAfWQpk5nwVKZve4u2E00a5iZP0aN');
     elements = stripe.elements();
 
     var style = {
